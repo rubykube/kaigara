b27c0950889f52145d7c6e1d3ca23a49cf80c3a7
MoD <mod@websys.io>
2016-09-08 19:10:05 +0200

    Adding kaigara provision Adding some usage documentation

--- cmd/exec.go
+++ cmd/exec.go
@@ -1,34 +1,25 @@
-// Copyright © 2016 Dimitry Koval <dkoval@heliostech.fr>
+// Copyright © 2016 Louis Bellet <mod@helios.sh>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 package cmd
 
 import (
-	"github.com/mod/kaigara/pkg/app"
 	"github.com/spf13/cobra"
+	"github.com/mod/kaigara/pkg/app"
 )
 
 // execCmd represents the exec command
 var execCmd = &cobra.Command{
 	Use:   "exec",
-	Short: "Execute a command or a binary",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Execute an executable in a child process",
+	Long: `This command can be used as an entrypoint, it will
+  search in $PATH for the executable and run it in a child
+  process`,
 	Run: func(cmd *cobra.Command, args []string) {
 		app.Execute(args[0], args[1:])
 	},
---
+++ cmd/provision.go
@@ -0,0 +1,55 @@
+// Copyright © 2016 Louis Bellet <mod@helios.sh>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+package cmd
+
+import (
+	"fmt"
+  "log"
+  "io/ioutil"
+  "path/filepath"
+
+	"github.com/spf13/cobra"
+	"github.com/mod/kaigara/pkg/app"
+)
+
+// provisionCmd represents the provision command
+var provisionCmd = &cobra.Command{
+	Use:   "provision",
+	Short: "Provisioning using operations",
+	Long: `Kaigara provision is a powerful system for maintaining container:
+
+  Step 1: Verify the state of operations using KAIGARA_INDEX
+  Step 2: Execute all pending operations using exec
+  Step 3: Update the index to reflect last operations`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("provision called")
+    runOperations()
+	},
+}
+
+func runOperations() {
+  path_ops := filepath.Join("opt","kaigara","operations")
+  files, err := ioutil.ReadDir(path_ops)
+
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  for _, file := range files {
+    fmt.Println("Found operation: " + file.Name())
+    app.Execute(filepath.Join(path_ops, file.Name()), nil)
+  }
+}
+
+func init() {
+	RootCmd.AddCommand(provisionCmd)
+
+  provisionCmd.Flags().BoolP("dry", "y", false, "Dry run operations")
+  provisionCmd.Flags().BoolP("verbose", "v", false, "Activate verbose")
+}
