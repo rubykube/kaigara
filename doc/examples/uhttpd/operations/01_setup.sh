e43f5da58518a211feb9a90fb7e9828b7e88d3c5
MoD <mod@websys.io>
2016-09-09 21:15:04 +0200

    Adding metafile to the example

--- doc/examples/uhttpd/operations/01_setup.sh
+++ doc/examples/uhttpd/operations/01_setup.sh
@@ -4,4 +4,4 @@ mkdir -p /var/lib/uhttpd/logs
 mkdir -p /var/lib/uhttpd/pids
 mkdir -p /var/lib/uhttpd/root
 
-kaigara render server.conf.tmpl /etc/uhttpd/server.conf
+kaigara render server.conf /etc/uhttpd/server.conf
