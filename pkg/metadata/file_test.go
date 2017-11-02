bcfa4a45c39d11e8610fb151cdd49812f0e8ec3d
Michael Contento <mail@michaelcontento.de>
2017-11-03 00:09:05 +0100

    Add support for nested metadata files

--- pkg/metadata/file_test.go
+++ pkg/metadata/file_test.go
@@ -41,6 +41,23 @@ func TestParseWithMerge(t *testing.T) {
 	os.RemoveAll(tmpdir)
 }
 
+func TestParseWithMergeNested(t *testing.T) {
+	tmpdir := test.MkTempDir(t)
+	os.Mkdir(tmpdir + "/nested/", 0750)
+	test.DumpFile(t, fmt.Sprintf("%s/defaults.yml", tmpdir), "test: data")
+	test.DumpFile(t, fmt.Sprintf("%s/nested/additionals.yaml", tmpdir), "info: data")
+
+	core.Init()
+	core.Set("core.path.metadata", tmpdir)
+
+	expected := Metamap{"test": "data", "info": "data"}
+	actual, err := Parse()
+	assert.Nil(t, err)
+	assert.Equal(t, expected, actual)
+
+	os.RemoveAll(tmpdir)
+}
+
 func TestParseWithGarbage(t *testing.T) {
 	tmpdir := test.MkTempDir(t)
 	test.DumpFile(t, fmt.Sprintf("%s/defaults.yml", tmpdir), "test: data")
